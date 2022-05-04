6f5a5b8ddb16db2d5117c230e0ae5bc8f1b5874a
tapiwajemwa <jemwataps@gmail.com>
2022-05-04 05:48:12 +0200

    Add project files.

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,46 @@
+﻿using HPlusSport.API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace HPlusSport.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly ShopContext _context;
+
+        public ProductsController(ShopContext context)
+        {
+            _context = context;
+            _context.Database.EnsureCreated();
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Product>>> GetAllProducts()
+        {
+            var products = await  _context.Products.ToListAsync();
+            return Ok(products);
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult> GetProduct(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+            if (product is null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> PostProduct(Product product)
+        {
+            _context.Products.Add(product);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction("GetProduct", new { id = product.Id },product);
+        }
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace HPlusSport.API.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string? Sku { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public decimal Price { get; set; }
+        public bool IsAvailable { get; set; }
+
+
+        [Required]
+        public int CategoryId { get; set; }
+        [JsonIgnore]
+        public virtual Category? Category { get; set; }
+    }
+}
